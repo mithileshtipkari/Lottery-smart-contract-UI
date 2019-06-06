a08ec84bb356144f267c3d6a6e870b0d6617bf87
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-06-07 00:00:15 +0530

    Initial commit

---
+++ src/Lottery.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Lottery.css';
+
+function App() {
+  return (
+    <div className="Lottery">
+      <header className="Lottery-header">
+        <h1>
+          Lottery Contract deployed on Rinkeby network
+        </h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
