8c522e06b3349faaf62b9e9214ff39dca6be8e08
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-06-08 23:47:20 +0530

    Changed lottery UI

--- src/Lottery.js
+++ src/Lottery.js
@@ -9,14 +9,6 @@ function App() {
         <h1>
           Lottery Contract deployed on Rinkeby network
         </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
