4574de1b574e53cff10809a18c7f4f4466172efe
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-06-16 18:15:52 +0530

    Changed Lottery UI

--- src/Lottery.js
+++ src/Lottery.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Lottery.css';
 import LoginCard from './LoginCard';
+import web3 from './web3';
 
-function App() {
-  return (
-    <div className="Lottery">
-        <h1>
-          Lottery Contract deployed on Rinkeby network
-        </h1>
-        <LoginCard/>
-    </div>
-  );
+class Lottery extends Component {
+  render(){
+    console.log(web3.version);
+    console.log(web3.currentProvider);
+    // web3.eth.getAccounts().then(console.log);
+    return (
+      <div className="Lottery">
+          <h1>
+            Lottery Contract deployed on Rinkeby network
+          </h1>
+          <LoginCard/>
+      </div>
+    );
+  };
 }
 
-export default App;
+export default Lottery;
