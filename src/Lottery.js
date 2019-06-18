3e51e23134c30e1e17a3c22ad6d88f5b944b49bd
mithilesht <mithilesht@cdac.in>
2019-06-18 20:14:02 +0530

    Fetched list of Players and Balance of Lottery

--- src/Lottery.js
+++ src/Lottery.js
@@ -7,18 +7,22 @@ import localLottery from './LocalLottery';
 class Lottery extends Component {
   constructor(props){
     super(props);
-    this.state = {manager: ''};
+    this.state ={manager: '',
+                  players: [],
+                  balance: ''
+                };
   }
 
   async componentDidMount(){
     console.log('mounted');
-    const manager = await localLottery.methods.manager().call();
-
-    this.setState({ manager });
+    const manager = await localLottery.methods.manager().call(); //fetch manager address
+    const players = await localLottery.methods.getPlayers().call(); //fetch list of players
+    const balance = await web3.eth.getBalance(localLottery.options.address); //fetch balance of contract
+    this.setState({ manager, players, balance });
   }
   render(){
-    console.log(web3.version);
-    console.log(web3.currentProvider);
+    // console.log(web3.version);
+    // console.log(web3.currentProvider);
     // web3.eth.getAccounts().then(console.log);
     return (
       <div className="Lottery">
@@ -27,6 +31,7 @@ class Lottery extends Component {
           </h1>
           <LoginCard/>
           <p>This contract is managed by {this.state.manager}</p>
+          <p>There are currently {this.state.players.length} people competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!</p>
       </div>
     );
   };
