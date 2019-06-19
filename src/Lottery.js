535221bfbcd83320d5d030356c3de0ef51410197
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-06-19 23:40:11 +0530

    Added some fix

--- src/Lottery.js
+++ src/Lottery.js
@@ -9,7 +9,8 @@ class Lottery extends Component {
     super(props);
     this.state ={manager: '',
                   players: [],
-                  balance: ''
+                  balance: '',
+                  value: ''
                 };
   }
 
@@ -19,6 +20,10 @@ class Lottery extends Component {
     const players = await localLottery.methods.getPlayers().call(); //fetch list of players
     const balance = await web3.eth.getBalance(localLottery.options.address); //fetch balance of contract
     this.setState({ manager, players, balance });
+  }
+
+  onSubmit = (event) => {
+
   }
   render(){
     // console.log(web3.version);
@@ -33,14 +38,19 @@ class Lottery extends Component {
           <p>This contract is managed by {this.state.manager}</p>
           <p>There are currently <strong>{this.state.players.length}</strong> people who are competing, to win <strong>{web3.utils.fromWei(this.state.balance, 'ether')}</strong> ether!</p>
           <hr/>
-          <form>
+          <form onSubmit={this.onSubmit()}>
               <h3>Wanna try your luck?</h3>
               <h4>Enter into this Lottery by sending 0.01 or more Ether</h4>
               <div>
-                  <label>Enter amount of Ether to enter lottery   </label>
+                  <label>Enter amount of Ether to enter lottery</label>
 
-                  <input placeholder="amount of ether"></input>
+                  <input
+                      placeholder="amount of ether"
+                      value={this.state.value}
+                      onChange={event => this.setState({value : event.target.value})}/>
               </div>
+              <p>The amount entered in Lottery is - {this.state.value}</p>
+              <button>Enter to Lottery</button>
           </form>
       </div>
     );
